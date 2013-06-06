e5446066d8f22ff65bb12a44ec791f9ccb2ff4a8
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2013-06-06 20:27:21 +0200

    Cleaned some code

--- leinpkg.py
+++ leinpkg.py
@@ -1,6 +1,11 @@
-import libxml2, re, sys
+
+"""
+This module parses a pom XML file in search project variables which
+can later be accessed through the Project class members
+"""
+
 import lxml.etree as et
-from subprocess import call
+
 
 class Project:
 
@@ -11,19 +16,28 @@ class Project:
     url = ""
 
     def __init__(self, pom_path):
+
         ns = {"pom": "http://maven.apache.org/POM/4.0.0"}
-
         tree = et.parse(open(pom_path))
-        self.name = tree.xpath('/pom:project/pom:name/text()', namespaces=ns)[0]
-        self.version = tree.xpath('/pom:project/pom:version/text()', namespaces=ns)[0]
 
-        dependencies = tree.xpath('/pom:project/pom:dependencies/pom:dependency/pom:artifactId/text()', namespaces=ns)
+        name_path = '/pom:project/pom:name/text()'
+        version_path = '/pom:project/pom:version/text()'
+        dependencies_path = ('/pom:project/pom:dependencies/pom:dependency/'
+                             'pom:artifactId/text()')
+        description_path = '/pom:project/pom:description/text()'
+        homepage_path = '/pom:project/pom:url/text()'
+
+        self.name = tree.xpath(name_path, namespaces=ns)[0]
+        self.version = tree.xpath(version_path, namespaces=ns)[0]
+
+        dependencies = tree.xpath(dependencies_path, namespaces=ns)
         if dependencies:
             self.dependencies = dependencies
-        description = tree.xpath('/pom:project/pom:description/text()', namespaces=ns)
+
+        description = tree.xpath(description_path, namespaces=ns)
         if description:
             self.description = description[0]
-        url = tree.xpath('/pom:project/pom:url/text()', namespaces=ns)
+
+        url = tree.xpath(homepage_path, namespaces=ns)
         if url:
             self.url = url[0]
-
