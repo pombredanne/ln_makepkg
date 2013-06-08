e4706c22b1f09a80d82744cc01b7911f3cab1415
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2013-06-08 22:18:38 +0200

    Added more info to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from distutils.core import setup
 
 setup(
-        name='ln_makepkg',
-        version='0.1',
-        scripts=['ln_makepkg'],
-        packages=['leinpkg'],
-        package_data = {
-            'leinpkg': ['templates/*']
-        }
-     )
+    name='ln_makepkg',
+    version='0.1',
+    scripts=['ln_makepkg'],
+    packages=['leinpkg'],
+    platforms=['any'],
+    description='Make debian packages from Leiningen project',
+    author="Eugenio Cano-Manuel Mendoza",
+    author_email="eugeniocanom@gmail.com",
+    url="https://github.com/keidesu/ln_makepkg",
+    package_data = {
+        'leinpkg': ['templates/*']
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python",
+        "Topic :: System :: Archiving :: Packaging",
+        "Topic :: Utilities",
+    ]
+ )
