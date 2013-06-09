82d32beadcb8d9b1be572460f676585fca82a1a4
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2013-06-09 19:34:32 +0200

    Added first test

---
+++ tests/files.py
@@ -0,0 +1,10 @@
+from leinpkg.tools import Project
+import os
+from subprocess import call
+
+
+def setup_module():
+    p = Project('tests/resources/pom.xml')
+
+def test_control_version():
+    assert True
