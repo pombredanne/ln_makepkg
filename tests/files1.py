c596e200b70107e2b627de993f08473b8d913f0d
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2013-06-10 17:33:41 +0200

    Finished setup_module

--- tests/files1.py
+++ tests/files1.py
@@ -1,12 +1,32 @@
-from leinpkg.tools import Project
+from subprocess import call
+import os
+import shutil
 
 
-project = None
+#Define what pom file belongs to this test.
+pom_file = 'pom1.xml'
+
+test_dir = os.path.abspath(os.path.dirname(__file__))
+temp_dir = test_dir + '/temp'
 
 
 def setup_module():
-    project = Project('tests/resources/pom.xml')
+    #Create a temporary folder where we can work in
+    if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
 
+    #Copy our pom file to the temporary directory
+    pom_src = test_dir + '/resources/' + pom_file
+    pom_dst = temp_dir + '/pom.xml'
+    shutil.copy(pom_src, pom_dst)
+
+    os.chdir(temp_dir)
+
+    call('python ' + test_dir + '/../ln_makepkg', shell=True)
+
+def teardown_module():
+    os.chdir(test_dir)
+    shutil.rmtree(test_dir)
 
 def test_control_version():
     assert True
