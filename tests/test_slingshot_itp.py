f981454862c79cfadbff63b4c973e5b962148730
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2013-06-14 22:57:09 +0200

    Set itp number by using command line argument

--- tests/test_slingshot_itp.py
+++ tests/test_slingshot_itp.py
@@ -7,5 +7,6 @@ class TestSlingshot(TestTemplates):
     _pom_file = 'pom_slingshot.xml'
     _expected_source_name = 'slingshot-clojure'
     _expected_package_name = 'libslingshot-clojure'
+    _commandline_args = '-i 699546'
     _expected_version = '0.10.3-1'
     _expected_itp_bug = '699546'
